644736f519e5a0e8f7c184d88106e215c7d2401f
Murilo Dias (Accer) <mdsouzadias@hotmail.com>
2025-04-22 08:46:29 -0300

    Implement add function for adding contacts to PhoneBook and update main program to handle ADD and SEARCH actions

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <cctype>
+#include <PhoneBook.hpp>
 
 void	toUpperCase(std::string &str)
 {
@@ -12,10 +13,32 @@ void	toUpperCase(std::string &str)
 	}
 }
 
+void	add(PhoneBook &phonebook)
+{
+	std::string first_name;
+	std::string last_name;
+	std::string nickname;
+	int phone_number;
+	std::string darkest_secret;
+
+	std::cout << "Type the first name" << std::endl;
+	std::cin >> first_name;
+	std::cout << "Type the last name" << std::endl;
+	std::cin >> last_name;
+	std::cout << "Type the nickname" << std::endl;
+	std::cin >> nickname;
+	std::cout << "Type the phone number" << std::endl;
+	std::cin >> phone_number;
+	std::cout << "Type the darkest secret" << std::endl;
+	std::cin >> darkest_secret;
+	phonebook.add(first_name, last_name, nickname, phone_number, darkest_secret);
+}
+
 int	main(int argc, char const *argv[])
 {
 	bool	exit = false;
 	std::string	action;
+	PhoneBook phonebook;
 
 	while (exit == false)
 	{
@@ -31,10 +54,13 @@ int	main(int argc, char const *argv[])
 		if (action == "ADD")
 		{
 			std::cout << "Add contact" << std::endl;
+			add(phonebook);
 		}
 		else if (action == "SEARCH")
 		{
 			std::cout << "Search for contact" << std::endl;
+			phonebook.showcontact(0);
+			phonebook.showcontact(1);
 		}
 		else if (action == "EXIT")
 			exit = true;
