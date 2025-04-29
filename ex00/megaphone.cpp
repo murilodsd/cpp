de45fbef8d2ecffb8be6000b3d8f768fb00fdcdf
Murilo Dias <mdsouzadias@hotmail.com>
2025-04-29 17:42:55 +0100

    Refactor megaphone program: extract toUpperCase function for better readability and maintainability

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -6,30 +6,43 @@
 /*   By: mde-souz <mde-souz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/20 10:55:51 by mde-souz          #+#    #+#             */
-/*   Updated: 2025/04/22 08:48:06 by mde-souz         ###   ########.fr       */
+/*   Updated: 2025/04/29 17:41:26 by mde-souz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cctype>
+#include <cstdlib>
+
+void	toUpperCase(std::string &str)
+{
+	size_t i = 0;
+
+	while (i < str.length())
+	{
+		str[i] = std::toupper(str[i]);
+		i++;
+	}
+}
 
 int main(int argc, char const *argv[])
 {
-	int	i;
-	int	j;
+	int		i;
+	std::string	str;
 
-	i = 1;
-	while (i < argc)
-	{
-		j = 0;
-		while (argv[i][j])
-		{
-			std::cout << (char)std::toupper(argv[i][j]);
-			j++;
-		}
-		i++;
-	}
 	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
-	std::cout << std::endl;
-	return (0);
+	else
+	{
+		i = 1;
+		while (i < argc)
+		{
+			str = argv[i];
+			toUpperCase(str);
+			std::cout << str;
+			i++;	
+		}		
+	}
+	std::cout << std::endl;	
+	return (EXIT_SUCCESS);
 }
