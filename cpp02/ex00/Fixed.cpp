e1feb2415bb3e9ae9f6eeaf2824e2f10d0845ce8
Murilo Dias (Accer) <mdsouzadias@hotmail.com>
2025-05-13 06:36:34 -0300

    Add Fixed class implementation with constructors, assignment operator, and member functions

---
+++ cpp02/ex00/Fixed.cpp
@@ -0,0 +1,58 @@
+#include <string>
+#include <iostream>
+#include "Fixed.hpp"
+
+// ================= Auxiliar Functions ================
+
+void	print_msg(const std::string& str, const std::string& color = WHITE)
+{
+	std::cout << color << str << std::endl << RESET;
+};
+
+// ==================== Constructor ====================
+Fixed::Fixed()
+:
+_fixedPointNumber(0)
+{
+	print_msg("Default constructor called");
+};
+
+// ================= Copy Constructor ==================
+Fixed::Fixed(const Fixed &other)
+{
+	print_msg("Copy constructor called");
+	*this = other;
+};
+
+// ========== Copy Assignment Operator Overload ==========
+Fixed &Fixed::operator=(const Fixed &other)
+{
+	if (this != &other)
+	{
+		print_msg("Copy assignment operator called");
+		this->_fixedPointNumber = other.getRawBits();
+	}
+	return *this;
+};
+
+// ==================== Destructor =====================
+Fixed::~Fixed()
+{
+	print_msg("Destructor called");
+};
+
+// ===================== Setters =======================
+
+void	Fixed::setRawBits(int const raw)
+{
+	print_msg("setRawBits member function called");
+	this->_fixedPointNumber = raw;
+};
+
+// ===================== Getters =======================
+
+int	Fixed::getRawBits(void) const
+{
+	print_msg("getRawBits member function called");
+	return (this->_fixedPointNumber);
+};
