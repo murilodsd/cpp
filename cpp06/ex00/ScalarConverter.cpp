d0a55ffa8faa40128f85451417c8de6d6245c46a
Murilo Dias (Accer) <mdsouzadias@hotmail.com>
2025-06-27 14:06:33 -0300

    checkDataType and convertSpecialCase functions done

--- cpp06/ex00/ScalarConverter.cpp
+++ cpp06/ex00/ScalarConverter.cpp
@@ -44,6 +44,22 @@ void convertInt(char c)
 	std::cout << "double: " << static_cast<double>(c) << std::endl;
 }
 
+void convertSpecialCase(const std::string& cpp_literal)
+{
+	std::cout << "char: impossible" << std::endl;
+	std::cout << "int: impossible" << std::endl;
+	if (cpp_literal == "nanf" || cpp_literal == "nan")
+	{
+		std::cout << "float: nanf" << std::endl;
+		std::cout << "double: nan" << std::endl;
+	}
+	else
+	{
+		std::cout << "float: " << cpp_literal[0] << "inff" << std::endl;
+		std::cout << "double: " << cpp_literal[0] << "inf" << std::endl;
+	}
+}
+
 
 // ================== Member Functions ==================
 
@@ -57,23 +73,76 @@ enum e_data_type
 	NONE
 };
 
+bool	isSpecialCase(const std::string& cpp_literal)
+{
+	std::string specialCases[6] = { "-inff", "+inff", "nanf", "-inf", "+inf", "nan"};
+
+	for (int i = 0; i < 6; i++)
+	{
+		if (cpp_literal == specialCases[i])
+			return true;
+	}
+	return (false);
+}
+
 e_data_type checkDataType(const std::string& cpp_literal)
 {
+	if (isSpecialCase(cpp_literal))
+		return (SPECIAL_CASE);
 	if (cpp_literal.length() == 1 && std::isalpha(cpp_literal.at(0)))
-		return CHAR;
-	
+		return (CHAR);
+	if (utils::is_str_number(cpp_literal, ""))
+		return (INT);
 	const char* ptr_cpp_literal = cpp_literal.c_str();
 	if (cpp_literal.at(0) == '+' || cpp_literal.at(0) == '-')
 		ptr_cpp_literal++;
-
-	if (utils::is_str_number(cpp_literal, ""))
+	if (std::isdigit(*ptr_cpp_literal))
+	{
+		while (std::isdigit(*ptr_cpp_literal))
+			ptr_cpp_literal++;
+		if (*ptr_cpp_literal == '.' && std::isdigit(ptr_cpp_literal[1]))
+		{
+			ptr_cpp_literal++;
+			while (std::isdigit(*ptr_cpp_literal))
+				ptr_cpp_literal++;
+			if (*ptr_cpp_literal == 'f' && ptr_cpp_literal[1] == '\0')
+				return (FLOAT);
+			else if (*ptr_cpp_literal == '\0')
+				return (DOUBLE);
+		}
+	}
+	return (NONE);
 }
 
 
 
 void ScalarConverter::convert(const std::string& cpp_literal)
 {
-	if (cpp_literal.length() == 1)
+	e_data_type dataType = checkDataType(cpp_literal);
+	
+	switch (dataType)
+	{
+		case CHAR:
+			std::cout << "Data type found: CHAR" << std::endl;
+			break;
+		case INT:
+			std::cout << "Data type found: INT" << std::endl;
+			break;
+		case FLOAT:
+			std::cout << "Data type found: FLOAT" << std::endl;
+			break;
+		case DOUBLE:
+			std::cout << "Data type found: DOUBLE" << std::endl;
+			break;
+		case SPECIAL_CASE:
+			std::cout << "Data type found: SPECIAL_CASE" << std::endl;
+			break;
+		case NONE:
+			std::cout << "Data type found: NONE" << std::endl;
+			break;
+	}
+	
+/* 	if (cpp_literal.length() == 1)
 	{
 		if (std::isalpha(cpp_literal.at(0)))
 			convertChar(cpp_literal.at(0));
@@ -81,5 +150,5 @@ void ScalarConverter::convert(const std::string& cpp_literal)
 			convertInt(std::atoi(cpp_literal.c_str()));
 	} 
 	else if (cpp_literal.length() == 1 and std::isalpha(cpp_literal.at(0)))
-		convertInt(std::atoi(cpp_literal.c_str()));
+		convertInt(std::atoi(cpp_literal.c_str())); */
 }
